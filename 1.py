7b3c162610ded875823dff3d329c44fe1d5c5de0
Sherry <13422066707@163.com>
2023-07-05 21:00:06 +0800

    2023.07.05，首次推送代码到git仓库上，好鸡冻好鸡冻

---
+++ 1.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+print('hello world')
+
+
+def Welcome():
+    print('今天是2023.06.19,第一次在家用visual code')
+
+Welcome()
+
+def jiafa(a,b):
+    return a+b
+x=5
+y=6
+z=jiafa(x,y)
+print(z)
